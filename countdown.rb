8a1d996dd1c152a1d5867a6a6fdee31e09479a0b
Andy Adams <andrewdcadams@gmail.com>
2020-07-07 21:27:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,4 +10,11 @@ def countdown(intArg)
 end
 
 def countdown_with_sleep(intArg)
-  
+  intArg = intArg.abs 
+  while intArg > 0 
+    puts "#{intArg} SECOND(S)!"
+    intArg -= 1 
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
